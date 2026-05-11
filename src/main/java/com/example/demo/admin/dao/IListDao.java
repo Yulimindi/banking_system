c39e3f0accc6de111fb7ad764404fb9301e3d2d0
Yulimindi <128598263+Yulimindi@users.noreply.github.com>
2026-05-11 14:09:38 +0900

    임시커밋2

--- src/main/java/com/example/demo/admin/dao/IListDao.java
+++ src/main/java/com/example/demo/admin/dao/IListDao.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import com.example.demo.admin.dto.ProductDto;
+import com.example.demo.admin.dto.AdminDto;
+import com.example.demo.personal.dto.UserDTO;
 
 @Mapper
 public interface IListDao {
 	AdminDto getAdmin(@Param("admin_id") Long admin_id);
 	List<AdminDto> getAdmins();
-	UserDto getUser(@Param("user_id") Long user_id);
-	List<UserDto> getUsers();
+	UserDTO getUser(@Param("user_id") Long user_id);
+	List<UserDTO> getUsers();
 }
