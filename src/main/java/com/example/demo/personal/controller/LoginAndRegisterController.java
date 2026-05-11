05416b7892e02503409a706237419aafd37ec3b1
gunyub <gunyub1120@naver.com>
2026-05-11 13:54:20 +0900

    feat: 회원가입중간

--- src/main/java/com/example/demo/personal/controller/LoginAndRegisterController.java
+++ src/main/java/com/example/demo/personal/controller/LoginAndRegisterController.java
@@ -1,8 +1,11 @@
-package com.example.demo.personal;
+package com.example.demo.personal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.example.demo.personal.dto.UserRegistDTO;
 
 @Controller
 public class LoginAndRegisterController {
@@ -13,7 +16,9 @@ public class LoginAndRegisterController {
 	}
 	
 	@PostMapping("/registerPro")
-	public String 회원가입() {
+	public String 회원가입(UserRegistDTO dto) {
+		System.out.println("회원가입 컨트롤러");
+		System.out.println(dto.toString());
 		return null;
 	}
 }
