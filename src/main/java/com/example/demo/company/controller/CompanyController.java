67789d4fe3fe33cfc4123be5f17281e97a6147fc
gunyub <gunyub1120@naver.com>
2026-05-13 12:53:07 +0900

    점심시간

--- src/main/java/com/example/demo/company/controller/CompanyController.java
+++ src/main/java/com/example/demo/company/controller/CompanyController.java
@@ -13,7 +13,7 @@ public class CompanyController {
         return "company/companyMain";
     }
 
-
+    
 
 
 
---
+++ src/main/java/com/example/demo/company/dao/ICompanyRegisterDAO.java
@@ -0,0 +1,17 @@
+package com.example.demo.company.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.demo.company.dto.CompanyUserDTO;
+import com.example.demo.personal.dto.Accounts_personalDTO;
+
+@Mapper
+public interface ICompanyRegisterDAO {
+
+	// 사업자 번호 조희
+	int selectBno(@Param("business_no") String bno);
+
+	int insertCompanyUser(CompanyUserDTO dto);
+
+}
