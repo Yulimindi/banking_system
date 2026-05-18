cffec274a70a3370fef69c97aac8ee8a9f4a645a
gunyub <gunyub1120@naver.com>
2026-05-18 12:01:47 +0900

    기업 메인페이지 받는거

--- src/main/java/com/example/demo/commonLogin/controller/LoginController.java
+++ src/main/java/com/example/demo/commonLogin/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.commonLogin.controller;
 
 import java.util.Map;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.commonLogin.dto.UserLoginDTO;
 import com.example.demo.commonLogin.service.LoginService;
+import com.example.demo.jwt.JwtUtil;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 public class LoginController {
 	
 	private final LoginService service;
-	
+	private final JwtUtil jwt;
 	@GetMapping("/loginPage")
 	public String 로그인페이지로이동() {
 		log.info("로그인페이지 컨트롤러");
@@ -33,8 +35,11 @@ public class LoginController {
 	public Map<String, String> 로그인(@RequestBody UserLoginDTO dto, HttpServletResponse response) {
 		Map<String, String> result =  service.login(dto);
 		if(result.containsKey("token")) {
+			String token = result.get("token");
 			log.debug("token : {}", result.get("token"));
-			
+			log.info("role {} 사용자 로그인 완료", jwt.getRole(result.get("token")));
+			String name = jwt.getUsername(token);
+			String role = jwt.getRole(token);
 			Cookie cookie = new Cookie("accessToken", result.get("token"));
 			
 			cookie.setHttpOnly(true);
@@ -42,7 +47,7 @@ public class LoginController {
 			cookie.setMaxAge(60*1);
 			response.addCookie(cookie);
 			
-			return Map.of("result", "sucess", "expiresIn", "60000");
+			return Map.of("result", "sucess", "role",role, "name",name);
 		}
 		return Map.of("result", "fail");
 	}
